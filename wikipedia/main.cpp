4eae5842b220098c2bb694b5a7258e16e65ace04
Dan Knoepfle <knoepfle@gmail.com>
2012-08-16 08:24:30 -0700

    Initial Commit

---
+++ wikipedia/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  wikipedia
+//
+//  Created by Daniel Knoepfle on 8/16/12.
+//  Copyright (c) 2012 Daniel Knoepfle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
