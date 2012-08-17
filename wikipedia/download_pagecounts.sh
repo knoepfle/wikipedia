f9a110b389cb3ca0888180841d1df5d9c806d452
Dan Knoepfle <knoepfle@gmail.com>
2012-08-17 02:02:31 -0700

    added a fix to handle MD5 checksumming on systems, like OS X, where md5sum isn't available but md5 is

--- wikipedia/download_pagecounts.sh
+++ wikipedia/download_pagecounts.sh
@@ -8,7 +8,9 @@ function get_year_months() {
     year=$1
     if [[ ! -e index-$year.html ]]
     then
-        curl http://dumps.wikimedia.org/other/pagecounts-raw/$year/ -o index-$year.html
+        echo -n "Downloading index for year $year: "
+        curl -s http://dumps.wikimedia.org/other/pagecounts-raw/$year/ -o index-$year.html
+        echo "done"
     fi
     sed -n '/>[0-9]\{4\}-[0-9]\{2\}</ s/.*>\([0-9]\{4\}-[0-9]\{2\}\)<.*/\1/ p' index-$year.html > months-$year.txt
 }
@@ -18,7 +20,9 @@ function get_month_files() {
     month=$2
     if [[ ! -e index-$month.html ]]
     then
-        curl http://dumps.wikimedia.org/other/pagecounts-raw/$year/$month/ -o index-$month.html
+        echo -n "Downloading index for month $month: "
+        curl -s http://dumps.wikimedia.org/other/pagecounts-raw/$year/$month/ -o index-$month.html
+        echo "done"
     fi
     sed -n '/\.gz/ s/.*>\(pagecounts[-0-9]*\.gz\)<.*/\1/ p' index-$month.html > files-$month.txt
 }
@@ -26,7 +30,7 @@ function get_month_files() {
 function download_month() {
     year=$1
     month=$2
-    echo "Downloading files for month $2"
+    echo "Downloading files for month $month"
     curl -s http://dumps.wikimedia.org/other/pagecounts-raw/$yyyy/$month/md5sums.txt -o md5sums-$month.txt
     for f in `cat files-$month.txt`
     do
@@ -39,17 +43,47 @@ function download_month() {
     done
 }
 
+function md5_check() {
+    year=$1
+    month=$2
+    f=$3
+    md5val=$(md5 -q $f)
+    grep -q "$md5val  $f" md5sums-$month.txt
+    return $?
+}
+
 function md5_check_month() {
     year=$1
     month=$2
     echo -n "Checking files for $month: "
     
-    md5invalid=$(md5sum --quiet -c md5sums-$month.txt 2>/dev/null)
-    
-    if [[ $? -ne 0 ]]
+    if command -v md5sum
+    then
+        ## system has md5sum; use it
+
+        md5invalid=$(md5sum --quiet -c md5sums-$month.txt 2>/dev/null)
+        md5status=$?
+        broken_files=$(echo "$md5invalid" | sed -n '/FAILED$/ s/\(pagecounts-[-0-9]*\.gz\):.*/\1/ p' )
+    else
+        ## no md5sum, probably running on Mac OS X; use md5 instead
+
+        md5status=0
+        for f in `cat files-$month.txt`
+        do
+            if [[ -e $f ]]
+            then
+                md5_check $year $month $f || broken_files="$broken_files $f" && let "md5status += 1"
+            else
+                let "md5status += 1"
+            fi
+        done
+        
+    fi
+
+    if [[ $md5status -ne 0 ]]
     then
         echo "FAILED"
-        broken_files=$(echo "$md5invalid" | sed -n '/FAILED$/ s/\(pagecounts-[-0-9]*\.gz\):.*/\1/ p' )
+        
         for b in $broken_files
         do
             echo "File $b has invalid MD5 checksum and has been deleted"
